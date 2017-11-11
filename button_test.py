24ee95380e5ec0ebe9921b862f7252254258d8f9
chasingbob <dries139@gmail.com>
2017-11-11 17:31:56 +0200

    updated button test

--- button_test.py
+++ button_test.py
@@ -3,6 +3,9 @@ from time import sleep
 
 button = 18
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 while True:
     input_state = GPIO.input(button)
     if input_state == True:
